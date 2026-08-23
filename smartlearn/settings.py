99783c7ddeec2683d3757911d26e1cbea011b4c7
Chan Myae Kyaw <chanmyaekyaw@Chans-MacBook-Air.local>
2026-08-24 01:14:16 +0630

    Add python-dotenv to requirements.txt

--- smartlearn/settings.py
+++ smartlearn/settings.py
@@ -9,11 +9,13 @@ import os
 import dj_database_url  # Used to parse Render's PostgreSQL URL
 
 
-from dotenv import load_dotenv
 import os
 
-load_dotenv()   # Loads variables from .env locally
-
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
